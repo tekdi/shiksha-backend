1466cae4f5f27e6a25a6d10fc3826617ab68a64a
Shubham <shubham.jha@tekditechnologies.com>
2024-03-23 14:21:10 +0530

    Task #216181: View Profile API with Custom Fields

--- src/common/database.module.ts
+++ src/common/database.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/user/entities/user-create-entity';
+import { User } from 'src/user/entities/user-entity';
 
 @Module({
   imports: [
---
+++ src/user/entities/user-entity.ts
@@ -0,0 +1,55 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({ name: "Users" })
+export class User {
+  @PrimaryColumn({ type: "uuid" })
+  userId: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  role: string;
+
+  @Column({ type: "date", nullable: true })
+  dob: Date;
+
+  @Column({ nullable: true })
+  email: string;
+
+  @Column({ nullable: true })
+  district: string;
+
+  @Column({ nullable: true })
+  state: string;
+
+  @Column({ nullable: true })
+  address: string;
+
+  @Column({ nullable: true })
+  pincode: string;
+
+  @CreateDateColumn({ type: "timestamp with time zone", default: () => "CURRENT_TIMESTAMP" })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: "timestamp with time zone", default: () => "CURRENT_TIMESTAMP" })
+  updatedAt: Date;
+
+//   @Column({ type: "numberic", nullable: true })
+//   mobile: number;
+
+  @Column({ nullable: true })
+  createdBy: string;
+
+  @Column({ nullable: true })
+  updatedBy: string;
+
+  @Column({ type: "uuid" })
+  tenantId: string;
+
+  @Column({ default: "active" })
+  status: string;
+}

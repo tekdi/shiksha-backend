3f03186699166055fc8df95b22c6ab75ffb37c36
Dhanashree-Patil95 <dhanashree.patil@tekditechnologies.com>
2024-04-08 18:23:56 +0530

    Fix[changes]

--- src/adapters/hasura/rbac/privilege.adapter.ts
+++ src/adapters/hasura/rbac/privilege.adapter.ts
@@ -1,7 +1,6 @@
 import { HttpService } from "@nestjs/axios";
 import { Injectable } from "@nestjs/common";
-import { RoleDto } from "../../../rbac/role/dto/role.dto";
-import { RoleSearchDto } from "../../../rbac/role/dto/role-search.dto";
+
 
 @Injectable()
 export class HasuraPrivilegeService {

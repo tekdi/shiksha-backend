af65dc27bf68649ad8adf7fc7774d25103a1e4a6
souravbhowmik1999 <sourav.bhowmik@tekditechnologies.com>
2024-04-16 11:10:56 +0530

    minor code changes

--- src/adapters/cohortservicelocator.ts
+++ src/adapters/cohortservicelocator.ts
@@ -4,9 +4,7 @@ import { CohortDto } from "src/cohort/dto/cohort.dto";
 
 export interface IServicelocatorcohort {
   createCohort(request: any, cohortDto: CohortCreateDto);
-  // getCohort(tenantId, cohortId, request, res);
   searchCohort(tenantid, request: any, cohortSearchDto: CohortSearchDto);
   updateCohort(cohortId: string, request: any, cohortDto: CohortCreateDto);
-  // getCohortList(tenantid,id,request,response);
-  updateCohortStatus(cohortId: string)
+  updateCohortStatus(cohortId: string);
 }

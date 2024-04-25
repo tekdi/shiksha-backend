f64830df9008bd5978400978a7ae07170085eb9f
apurvaubade <apurva.bade@tekditechnologies.com>
2024-04-25 13:42:13 +0530

    Privilege:Delete privilege API

--- src/adapters/privilegeservicelocator.ts
+++ src/adapters/privilegeservicelocator.ts
@@ -1,12 +1,12 @@
-import { CreatePrivilegesDto, PrivilegeDto } from "src/rbac/privilege/dto/privilege.dto";
+import {
+  CreatePrivilegesDto,
+  PrivilegeDto,
+} from "src/rbac/privilege/dto/privilege.dto";
 
 export interface IServicelocator {
-    createPrivilege(loggedinUser: any, createPrivileges: CreatePrivilegesDto);
-    getPrivilege(
-        privilegeId?: string,
-        request?: any,
-    );
-    // updatePrivilege(privilegeId, request, privilegeDto)
-    getAllPrivilege(request)
-    deletePrivilege(privilegeId, request)
+  createPrivilege(loggedinUser: any, createPrivileges: CreatePrivilegesDto);
+  getPrivilege(privilegeId?: string, request?: any);
+  // updatePrivilege(privilegeId, request, privilegeDto)
+  getAllPrivilege(request);
+  deletePrivilege(privilegeId);
 }

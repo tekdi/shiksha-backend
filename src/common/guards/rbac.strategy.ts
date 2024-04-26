8e482722dc8440b7f46e75802006c589564aae4d
Kshitija Kadam <kshitijakadam04@gmail.com>
2024-04-26 22:15:21 +0530

    Task #218147 feat : checked iss and aud valid

--- src/common/guards/rbac.strategy.ts
+++ src/common/guards/rbac.strategy.ts
@@ -1,11 +1,11 @@
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { PassportStrategy } from "@nestjs/passport";
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class RbacJwtStrategy extends PassportStrategy(Strategy, "jwt-rbac") {
-  constructor(configService: ConfigService) {
+  constructor(private configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromHeader("rbac_token"),
       ignoreExpiration: false,
@@ -14,12 +14,14 @@ export class RbacJwtStrategy extends PassportStrategy(Strategy, "jwt-rbac") {
   }
 
   async validate(payload: any) {
-    /**
-     * This can be obtained via req.user in the Controllers
-     * This is where we validate that the user is valid and delimit the payload returned to req.user
-     */
-
-    // console.log(payload, "payload -rbac");
+    if (
+      !(
+        payload.iss === this.configService.get<string>("ISSUER") &&
+        payload.aud === this.configService.get<string>("AUDIENCE")
+      )
+    ) {
+      throw new UnauthorizedException();
+    }
     return payload;
   }
 }

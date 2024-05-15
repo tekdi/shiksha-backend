295de9c61578cd51a5887775741dbc8d012713a0
Dhanashree-Patil95 <dhanashree.patil@tekditechnologies.com>
2024-05-15 16:21:52 +0530

    Task:PS-408 Attendance:Added sort functionality to data from attendance/list API

--- src/attendance/dto/attendance-search.dto.ts
+++ src/attendance/dto/attendance-search.dto.ts
@@ -1,13 +1,13 @@
-  import { ApiAcceptedResponse, ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
-  import { IsArray, IsNotEmpty, IsNumber, IsOptional, IsString, IsUUID, Matches, Validate, ValidateIf, ValidateNested, ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface, isNotEmpty } from "class-validator";
-  import { isBefore, isSameDay } from "date-fns";
-  import { UserDto } from "src/user/dto/user.dto";
-  import { AttendanceDto } from "./attendance.dto";
-  import { CohortMembersDto } from "src/cohortMembers/dto/cohortMembers.dto";
-  import { Type } from "class-transformer";
+import { ApiAcceptedResponse, ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+import { ArrayMaxSize, ArrayMinSize, IsArray, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString, IsUUID, Matches, Validate, ValidateIf, ValidateNested, ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface, isNotEmpty } from "class-validator";
+import { isBefore, isSameDay } from "date-fns";
+import { UserDto } from "src/user/dto/user.dto";
+import { AttendanceDto } from "./attendance.dto";
+import { CohortMembersDto } from "src/cohortMembers/dto/cohortMembers.dto";
+import { Type } from "class-transformer";
 
 
-  @ValidatorConstraint({ name: 'isNotAfterFromDate', async: false })
+@ValidatorConstraint({ name: 'isNotAfterFromDate', async: false })
 export class IsFromDateBeforeToDateConstraint implements ValidatorConstraintInterface {
   validate(fromDate: Date, args: ValidationArguments) {
     const toDate = args.object[args.constraints[0]];
@@ -20,102 +20,124 @@ export class IsFromDateBeforeToDateConstraint implements ValidatorConstraintInte
   }
 }
 
-  export class AttendanceFiltersDto  {
-    @ApiPropertyOptional({default:"yyyy-mm-dd"})
-    @IsOptional()
-    @Matches(/^\d{4}-\d{2}-\d{2}$/, { message: 'Please provide a valid date in the format yyyy-mm-dd' })
-    @Validate(IsFromDateBeforeToDateConstraint, ['toDate'])
-    fromDate?: Date;
 
-    @ApiPropertyOptional({default:"yyyy-mm-dd"})
-    @Matches(/^\d{4}-\d{2}-\d{2}$/, { message: 'Please provide a valid date in the format yyyy-mm-dd' })
-    @IsOptional()
-    toDate?: Date;
+enum SortDirection {
+  ASC = 'asc',
+  DESC = 'desc',
+}
 
-    @ApiPropertyOptional()
-    @IsUUID()
-    @IsOptional()
-    contextId ?:string
+export class AttendanceFiltersDto {
+  @ApiPropertyOptional({ default: "yyyy-mm-dd" })
+  @IsOptional()
+  @Matches(/^\d{4}-\d{2}-\d{2}$/, { message: 'Please provide a valid date in the format yyyy-mm-dd' })
+  @Validate(IsFromDateBeforeToDateConstraint, ['toDate'])
+  fromDate?: Date;
 
-    @ApiPropertyOptional()
-    scope ?: string
+  @ApiPropertyOptional({ default: "yyyy-mm-dd" })
+  @Matches(/^\d{4}-\d{2}-\d{2}$/, { message: 'Please provide a valid date in the format yyyy-mm-dd' })
+  @IsOptional()
+  toDate?: Date;
 
-    @ApiPropertyOptional({
-      type: String,
-      description: "The date of the attendance in format yyyy-mm-dd",
-      default:"yyyy-mm-dd"
-    })
-    @IsOptional()
-    @Matches(/^\d{4}-\d{2}-\d{2}$/, { message: 'Please provide a valid date in the format yyyy-mm-dd' })
-    attendanceDate ?:Date
+  @ApiPropertyOptional()
+  @IsUUID()
+  @IsOptional()
+  contextId?: string
 
+  @ApiPropertyOptional()
+  scope?: string
 
-    @ApiPropertyOptional()
-    @IsUUID()
-    @IsOptional()
-    userId?: string
-
-    // @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    type: String,
+    description: "The date of the attendance in format yyyy-mm-dd",
+    default: "yyyy-mm-dd"
+  })
+  @IsOptional()
+  @Matches(/^\d{4}-\d{2}-\d{2}$/, { message: 'Please provide a valid date in the format yyyy-mm-dd' })
+  attendanceDate?: Date
 
 
-    [key: string]: any; // Allows additional dynamic keys
-  }
+  @ApiPropertyOptional()
+  @IsUUID()
+  @IsOptional()
+  userId?: string
 
-  @ValidatorConstraint({ name: "validDateRange", async: false })
-  export class ValidDateRangeConstraint implements ValidatorConstraintInterface {
-    validate(value: any, args: ValidationArguments) {
-      const { toDate, fromDate } = value;
+  // @ApiPropertyOptional()
 
-      // If either fromDate or toDate is not provided, validation passes
-      if (!fromDate || !toDate) {
-        return true;
-      }
 
-      return isBefore(new Date(fromDate), new Date(toDate)) || isSameDay(new Date(fromDate), new Date(toDate));
+  [key: string]: any; // Allows additional dynamic keys
+}
+
+@ValidatorConstraint({ name: "validDateRange", async: false })
+export class ValidDateRangeConstraint implements ValidatorConstraintInterface {
+  validate(value: any, args: ValidationArguments) {
+    const { toDate, fromDate } = value;
+
+    // If either fromDate or toDate is not provided, validation passes
+    if (!fromDate || !toDate) {
+      return true;
     }
 
-    defaultMessage(args: ValidationArguments) {
-      return "Invalid date range. 'toDate' must be after or equal to 'fromDate'.";
-    }
+    return isBefore(new Date(fromDate), new Date(toDate)) || isSameDay(new Date(fromDate), new Date(toDate));
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return "Invalid date range. 'toDate' must be after or equal to 'fromDate'.";
+  }
+}
+
+
+
+export class AttendanceSearchDto {
+  @ApiPropertyOptional({
+    type: Number,
+    description: "Limit",
+  })
+  @IsOptional()
+  @IsNotEmpty()
+  @IsNumber({}, { message: 'Limit must be a number' })
+  limit: number;
+
+  @ApiProperty({
+    type: Number,
+    description: "number",
+  })
+  page: number;
+
+  @ApiPropertyOptional({
+    type: AttendanceFiltersDto,
+    description: "Filters",
+  })
+  @ValidateNested({ each: true })
+  @Type(() => AttendanceFiltersDto)
+  // @Validate(ValidDateRangeConstraint, { message: "Invalid date range." })
+  filters: AttendanceFiltersDto
+
+
+  @ApiPropertyOptional({
+    description: "Facets",
+    example: ["contextId", "userId", "scope"]
+  })
+  facets?: string[];
+
+  @ApiPropertyOptional({
+    description: "Sort",
+    example: ["attendanceDate", "ascending"]
+  })
+  @IsArray()
+  @IsOptional()
+  @ArrayMinSize(2, { message: 'Sort array must contain exactly two elements' })
+  @ArrayMaxSize(2, { message: 'Sort array must contain exactly two elements' })
+  sort: [string, string];
+
+  @ValidateIf((o) => o.sort !== undefined)
+  @IsEnum(SortDirection, { each: true, message: 'Sort[1] must be either asc or desc' })
+  get sortDirection(): string | undefined {
+    return this.sort ? this.sort[1] : undefined;
   }
 
 
 
-  export class AttendanceSearchDto {
-    @ApiPropertyOptional({
-      type: Number,
-      description: "Limit",
-    })
-    @IsOptional()
-    @IsNotEmpty() 
-    @IsNumber({}, { message: 'Limit must be a number' }) 
-    limit: number;
-
-    @ApiProperty({
-      type: Number,
-      description: "number",
-    })
-    page: number;
-
-    @ApiPropertyOptional({
-      type: AttendanceFiltersDto,
-      description: "Filters",
-    })
-    @ValidateNested({ each: true })
-    @Type(() => AttendanceFiltersDto)
-    // @Validate(ValidDateRangeConstraint, { message: "Invalid date range." })
-    filters: AttendanceFiltersDto
-
-
-    @ApiPropertyOptional({
-      description: "Facets",
-      example: [ "contextId","userId","scope"]
-    })
-    facets?: string[];
-
-
-
-    constructor(partial: Partial<AttendanceSearchDto>) {
-      Object.assign(this, partial);
+  constructor(partial: Partial<AttendanceSearchDto>) {
+    Object.assign(this, partial);
   }
-  }
+}

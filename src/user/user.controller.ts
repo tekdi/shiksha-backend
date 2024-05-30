64986549d6ea22da9a4ca83cb04e85a73f758b57
Kshitija Kadam <kshitijakadam04@gmail.com>
2024-05-30 20:59:02 +0530

    Task #220234 : chore - minor fix

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -187,6 +187,7 @@ export class UserController {
   }
 
    //delete
+   @UseFilters(new AllExceptionsFilter(APIID.USER_DELETE))
    @Delete("delete/:userId")
    @UseGuards(JwtAuthGuard)
    @ApiBasicAuth("access-token")

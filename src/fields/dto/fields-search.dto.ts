b4bb862b4429282e22f025c2467e31777f54f96d
souravbhowmik1999 <sourav.bhowmik@tekditechnologies.com>
2024-06-06 15:37:22 +0530

    add

--- src/fields/dto/fields-search.dto.ts
+++ src/fields/dto/fields-search.dto.ts
@@ -1,6 +1,4 @@
 import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
-import { Expose } from "class-transformer";
-import { IsNotEmpty } from "class-validator";
 
 export class FieldsSearchDto {
   @ApiProperty({
